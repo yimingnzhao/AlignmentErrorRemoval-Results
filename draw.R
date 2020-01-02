22d214b19ec791b91e7d7cd919ed4745d4ccb94d
yimingnzhao <90603yahoo@gmail.com>
2020-01-01 21:27:29 -0800

    added save figures commands

--- draw.R
+++ draw.R
@@ -1,17 +1,21 @@
 require(ggplot2); require(scales); require(reshape2)
-d=(read.csv("~/Linux Files/AlignmentErrorRemoval-TestingPipeline/3K_GeneralResults/res.csv", sep=",", header=F))
+d=(read.csv("~/Linux Files/AlignmentErrorRemoval-Results/res.csv", sep=",", header=F))
 names(d) <- c("E", "DR", "X", "Diameter", "PD", "N", "ErrLen", "NumErrSeqDiv", "Rep", "FP0", "FN0", "TP0", "TN0", "FP", "FN", "TP", "TN")
 
 # General Results: Recall vs Diameter
-ggplot(aes(x=Diameter, y=TP/(TP+FN)), data=d[d$E=="16S.B" & d$N > 10 & d$ErrLen=="X",]) + theme_classic() + geom_point() + geom_smooth() + scale_y_continuous("Recall")
-ggplot(aes(x=Diameter, y=TP/(TP+FN)), data=d[d$E=="Hackett" & d$N > 10 & d$ErrLen=="X",]) + theme_classic() + geom_point() + geom_smooth() + scale_y_continuous("Recall")
-ggplot(aes(x=Diameter, y=TP/(TP+FN)), data=d[d$E=="small-10-aa-RV100-BBA0039" & d$N > 10 & d$ErrLen=="X",]) + theme_classic() + geom_point() + geom_smooth() + scale_y_continuous("Recall")
+ggplot(aes(x=Diameter, y=TP/(TP+FN)), data=d[d$E=="16S.B" & d$N > 10 & d$ErrLen==0,]) + theme_classic() + geom_point() + geom_smooth() + scale_y_continuous("Recall") + ggtitle("16S.B: Recall vs Diameter")
+ggsave("16SB_General_Recall.png", width=6, height=6)
+ggplot(aes(x=Diameter, y=TP/(TP+FN)), data=d[d$E=="Hackett" & d$N > 10 & d$ErrLen==0,]) + theme_classic() + geom_point() + geom_smooth() + scale_y_continuous("Recall") + ggtitle("Hackett: Recall vs Diameter")
+ggsave("Hackett_General_Recall.png", width=6, height=6)
+ggplot(aes(x=Diameter, y=TP/(TP+FN)), data=d[d$E=="small-10-aa-RV100-BBA0039" & d$N > 10 & d$ErrLen==0,]) + theme_classic() + geom_point() + geom_smooth() + scale_y_continuous("Recall") + ggtitle("small-10-aa: Recall vs Diameter")
+ggsave("small10aa_General_Recall.png", width=6, height=6)
 
 # Recall vs Diameter: 
-ggplot(aes(x=Diameter,y=TP/(TP+FN),color=as.factor(ErrLen)),data=d[d$E=="16S.B" & d$N > 10 & d$ErrLen!="X",])+
-  geom_point(alpha=0.5)+
-  theme_classic()+geom_smooth()+scale_y_continuous("Recall")+
-  scale_shape(name="")+scale_color_brewer(palette = "Paired",name="error len", labels = function(x) (paste(x, intToUtf8(215), "11")))
+ggplot(aes(x=Diameter,y=TP/(TP+FN),color=as.factor(ErrLen)),data=d[d$E=="16S.B" & d$N > 10 & d$ErrLen!=0,])+
+  geom_point(alpha=0.1)+theme_classic()+geom_smooth(se=F)+scale_y_continuous("Recall")+
+  scale_shape(name="")+scale_color_brewer(palette = "Paired",name="error len", labels = function(x) (paste(x, intToUtf8(215), "11")))+
+  ggtitle("16S.B with Specific Error Lengths: Recall vs Diameter")
+ggsave("16SB_ErrLen_Recall.png")
 
 # Aggregate Sum function
 summ_roc <- function(d2,form) {
@@ -30,40 +34,30 @@ summ_roc <- function(d2,form) {
 }
 
 options(digits = 2)
-d2=summ_roc(d[d$E=="16S.B" & d$N > 19 & d$ErrLen!="X",], ErrLen+cut(Diameter, breaks = c(0, 0.1, 0.2, 0.5, 0.8, 1), right = F)~.)
+d2=summ_roc(d[d$E=="16S.B" & d$N > 19 & d$ErrLen!=0,], ErrLen+cut(Diameter, breaks = c(0, 0.1, 0.2, 0.5, 0.8, 1), right = F)~.)
 A = data.frame(x=d2$FP/(d2$FP+d2$TN),y=d2$TP/(d2$TP+d2$FN), ErrLen=d2$ErrLen, DR=d2$`cut(Diameter, breaks = c(0, 0.1, 0.2, 0.5, 0.8, 1), right = F)`)
 B = data.frame(x=d2$FP0/(d2$FP0+d2$TN0),y=as.vector(matrix(1.1,nrow=nrow(d2))), ErrLen=d2$ErrLen, DR=d2$`cut(Diameter, breaks = c(0, 0.1, 0.2, 0.5, 0.8, 1), right = F)`)
 ggplot(data=A, aes(x, y, color=as.factor(ErrLen), shape=as.factor(DR))) + geom_point(alpha=1)+
   theme_light()+theme(legend.position = "right")+geom_point(data=B)+
   scale_shape(name="Diameter")+scale_color_brewer(name="Error Length",palette = "Paired",labels = function(x) (paste(x, intToUtf8(215), "11")))+
   scale_x_continuous(name="FPR",labels=percent)+
-  scale_y_continuous("Recall",labels=percent,breaks = c(0.2,0.4,0.6,0.8,1))+coord_cartesian(xlim=c(0, 0.0015), ylim=c(0,1.15))
-
-# 16S.B: ErrLen - Recall vs FPR (sum) [includes error length and percentage of sequences with error]
-ad3 =  summ_roc(d2,n~.) 
-ad3$var=8
-ad3 = rbind(ad3,data.frame(n=5,summ_roc(d[d$E=="16S.B_ErrLen",],var~.))[,c(1,3:6,2)])
-ggplot(aes(x=FP/(FP+TN),y=TP/(TP+FN),color=as.factor(var),shape=as.factor(n)),data=ad3)+
-  geom_point(alpha=1)+
-  theme_light()+theme()+
-  scale_shape(name="n",labels=nlabels)+
-  scale_color_brewer(name="error len",palette = "Paired",labels = function(x) (paste(x,"k")))+
-  scale_x_continuous(name="FPR",labels=percent)+
-  scale_y_continuous("Recall",labels=percent,breaks = c(0.2,0.4,0.6,0.8,1))+coord_cartesian(ylim=c(0.2,1))
-ggsave("sum-len-n.pdf",width=4.5,height = 4)
-
-
+  scale_y_continuous("Recall",labels=percent,breaks = c(0.2,0.4,0.6,0.8,1))+
+  ggtitle("16S.B with Specific Error Lengths: ROC")
+ggsave("16SB_ErrLen_ROC.png", width=6, height=6)
 
 # ROC for General 16S.B
 options(digits = 5)
-d2=summ_roc(d[d$E=="16S.B" & d$N > 10 & d$ErrLen=="X",], ErrLen+cut(Diameter, breaks = c(0, 0.1, 0.2, 0.5, 0.8, 1), right = F)~.)
+d2=summ_roc(d[d$E=="16S.B" & d$N > 10 & d$ErrLen==0,], ErrLen+cut(Diameter, breaks = c(0, 0.1, 0.2, 0.5, 0.8, 1), right = F)~.)
 A = data.frame(x=d2$FP/(d2$FP+d2$TN),y=d2$TP/(d2$TP+d2$FN), ErrLen=d2$ErrLen, DR=d2$`cut(Diameter, breaks = c(0, 0.1, 0.2, 0.5, 0.8, 1), right = F)`)
 B = data.frame(x=d2$FP0/(d2$FP0+d2$TN0),y=as.vector(matrix(1.1,nrow=nrow(d2))), ErrLen=d2$ErrLen, DR=d2$`cut(Diameter, breaks = c(0, 0.1, 0.2, 0.5, 0.8, 1), right = F)`)
 ggplot(data=A, aes(x, y, color=as.factor(DR))) + geom_point(alpha=1)+
   theme_light()+theme(legend.position = "right")+geom_point(data=B)+
   scale_color_brewer(name="Diameter",palette = "Paired",labels = function(x) (paste(x)))+
   scale_x_continuous(name="FPR",labels=percent)+
-  scale_y_continuous("Recall",labels=percent,breaks = c(0.2,0.4,0.6,0.8,0.9,0.95,1,1.1))#+coord_cartesian(xlim=c(0,0.00015), ylim=c(0.9,1.15))
+  scale_y_continuous("Recall",labels=percent,breaks = c(0.2,0.4,0.6,0.8,0.9,0.95,1,1.1))+
+  ggtitle("16S.B: ROC")
+ggsave("16SB_General_ROC.png", width=6, height=6)
+
 
 # ROC for General Hackett
 options(digits = 5)
@@ -74,7 +68,10 @@ ggplot(data=A, aes(x, y, color=as.factor(DR))) + geom_point(alpha=1)+
   theme_light()+theme(legend.position = "right")+geom_point(data=B)+
   scale_color_brewer(name="Diameter",palette = "Paired",labels = function(x) (paste(x)))+
   scale_x_continuous(name="FPR",labels=percent)+
-  scale_y_continuous("Recall",labels=percent,breaks = c(0.2,0.4,0.6,0.8,0.9,0.95,1,1.1))#+coord_cartesian(xlim=c(0,0.00015), ylim=c(0.9,1.15))
+  scale_y_continuous("Recall",labels=percent,breaks = c(0.2,0.4,0.6,0.8,0.9,0.95,1,1.1))+
+  ggtitle("Hackett: ROC")
+ggsave("Hackett_General_ROC.png", width=6, height=6)
+
 
 # ROC for General small-10-aa
 options(digits = 5)
@@ -85,6 +82,8 @@ ggplot(data=A, aes(x, y, color=as.factor(DR))) + geom_point(alpha=1)+
   theme_light()+theme(legend.position = "right")+geom_point(data=B)+
   scale_color_brewer(name="Diameter",palette = "Paired",labels = function(x) (paste(x)))+
   scale_x_continuous(name="FPR",labels=percent)+
-  scale_y_continuous("Recall",labels=percent,breaks = c(0.2,0.4,0.6,0.8,0.9,0.95,1,1.1))#+coord_cartesian(xlim=c(0,0.00015), ylim=c(0.9,1.15))
+  scale_y_continuous("Recall",labels=percent,breaks = c(0.2,0.4,0.6,0.8,0.9,0.95,1,1.1))+ggtitle("small-10-aa: ROC")
+ggsave("small10aa_General_ROC.png", width=6, height=6)
+
 
 
